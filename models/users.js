0b90a191271fca8dad1fa84e042b9d9a7fdb0ed9
bagussr <bagusssyamsu@gmail.com>
2022-11-13 09:12:05 +0700

    initial project

---
+++ models/users.js
@@ -0,0 +1,22 @@
+const { Sequlize, DataTypes } = require('sequelize');
+const sequelize = new Sequlize('sqlite::memory:');
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
